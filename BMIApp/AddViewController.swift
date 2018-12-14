a7e7857dcee6838cdc7fc1b0371298b27e22cd80
sree-86 <sriram3rhyme@gmail.com>
2018-12-13 20:30:01 -0500

    implement bmi functions

--- BMIApp/AddViewController.swift
+++ BMIApp/AddViewController.swift
@@ -31,6 +31,7 @@ class AddViewController: UIViewController, UITextFieldDelegate {
         
         let currentValue = Float(sender.value)
         heightText.text = "\(currentValue) centimetres"
+        self.calculateBMI()
     }
     
     
@@ -38,10 +39,40 @@ class AddViewController: UIViewController, UITextFieldDelegate {
         
         let currentValue = Float(sender.value)
         weightText.text = "\(currentValue) kilograms"
+        self.calculateBMI()
     }
     
     
     
+    private func calculateBMI() {
+        let height: Float = heightSlider.value
+        let htmetre = height/100
+        let weight: Float = weightSlider.value
+        let bmi: Float = (weight / (htmetre*htmetre))
+        
+        BMIValue.text = "\(bmi)"
+        self.changeStatus(bmi: bmi)
+    }
+    
+    private func changeStatus(bmi: Float) {
+        if (bmi < 16) {
+            statusLabel.text = "Severe Thinness"
+        } else if (bmi >= 16 && bmi < 17) {
+            statusLabel.text = "Moderate Thinness"
+        } else if (bmi >= 17 && bmi < 18.5) {
+            statusLabel.text = "Mild Thinness"
+        } else if (bmi >= 18.5 && bmi < 25) {
+            statusLabel.text = "Normal"
+        } else if (bmi >= 25 && bmi < 30) {
+            statusLabel.text = "Overweight"
+        } else if (bmi >= 30 && bmi < 35) {
+            statusLabel.text = "Obese Class I"
+        } else if (bmi >= 35 && bmi < 40) {
+            statusLabel.text = "Obese Class II"
+        }  else {
+            statusLabel.text = "Obese Class III"
+        }
+    }
 
 
 }
